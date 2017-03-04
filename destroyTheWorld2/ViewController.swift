6df37c94ac30eef92d7bd888d0f8b1236b8b2bd1
Habibur Rahman <Habibur@Habiburs-Mac-mini.local>
2017-03-04 17:37:25 +0600

    Initial Commit

---
+++ destroyTheWorld2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroyTheWorld2
+//
+//  Created by Habibur Rahman on 3/4/17.
+//  Copyright © 2017 Habibur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
